8d13f1325746385c964532be7209d5e32064cdf8
Rodolfo Bittkau <tyrmund@gmail.com>
2025-07-21 21:46:19 +0200

    Added TailwindCSS

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <nav>
+    <div class="logo">EDP Front</div>
+    <ul class="nav-links">
+        <li>Platform</li>
+        <li>Solutions</li>
+        <li>Compliance</li>
+        <li>Clients</li>
+        <li>Insights</li>
+    </ul>
+    <button class="cta">Request a Demo</button>
+    </nav>
+</template>
